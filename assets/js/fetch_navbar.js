a1de6cbcdbb597dde19b0defccbdcfe7daa11dca
Olivier <olivier.gabelle1@gmail.com>
2025-01-29 13:21:37 +0100

    feature: added models and more content

--- assets/js/fetch_navbar.js
+++ assets/js/fetch_navbar.js
@@ -1,4 +1,4 @@
-fetch('components/navbar.html')
+fetch('../components/navbar.html')
 .then(response => response.text())
 .then(data => {
   document.getElementById('navbar-container').innerHTML = data;
